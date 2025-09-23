e59c2fcbcb9b21592dc47a4bd2bf2c99bfa31584
root <root@ip-172-31-2-79.ap-southeast-2.compute.internal>
2025-09-23 02:49:13 +0000

    var block for instance type

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "instance_type_name" {
+  type = string
+  default = "t2.micro"
+}
+
 resource "aws_instance" "demo_instance" {
-  instance_type = "t2.micro"
+  instance_type = var.instance_type_name
   ami           = "ami-12345"
 }
