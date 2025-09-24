281c6404e082b3185df2c96a6fe3e8b702177630
root <root@ip-172-31-2-79.ap-southeast-2.compute.internal>
2025-09-24 03:12:01 +0000

    Include a variable block for region

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region_name
 }
 
 variable "instance_type_name" {
@@ -7,6 +7,10 @@ variable "instance_type_name" {
 }
 
 variable "instance_image" {
+  type = string
+}
+
+variable "region_name" {
   type = string
 }
 
