cae185f60513348b42eeac213098070826526160
Rabi A <41875211+Dgruploads@users.noreply.github.com>
2025-09-24 08:46:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,5 +24,5 @@ output "instance_public_ip_addr" {
 }
 
 output "instance_availability_zone" {
-  value = aws_instance.example_terraform.availability_zone
+  value = aws_instance.demo_instance.availability_zone
 }
